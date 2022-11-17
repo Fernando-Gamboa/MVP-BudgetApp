3927fe6f5c5da2a85cb1cdcd303886b32c7aff65
Fernando Gamboa <103979716+Fernando-Gamboa@users.noreply.github.com>
2022-11-16 16:50:23 -0800

    Add repo via upload

---
+++ MVP_budget_app/my-app/src/components/TransactionsGoalsEntry.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const TransactionsGoalsEntry = (props) => {
+
+  return (
+    <div>
+      {props.entry.amount}
+    </div>
+  )
+}
+
+export default TransactionsGoalsEntry;
